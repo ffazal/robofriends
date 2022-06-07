33ccf47c8b18516982e2329b78a5dc9a0643cc14
Faizan Fazal <39065186+ffazal@users.noreply.github.com>
2022-06-07 14:13:57 -0400

    Updates

---
+++ precache-manifest.6148a904295e0f7bad52f46a2c446e42.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7eebd5f4ad8de73a049d",
+    "url": "/static/css/main.c333b150.chunk.css"
+  },
+  {
+    "revision": "7eebd5f4ad8de73a049d",
+    "url": "/static/js/main.a1b14cda.chunk.js"
+  },
+  {
+    "revision": "e4686e9a92b21e222891",
+    "url": "/static/js/runtime~main.99c07c0c.js"
+  },
+  {
+    "revision": "b47fe71883e4a41ec331",
+    "url": "/static/css/2.c3e5aa5d.chunk.css"
+  },
+  {
+    "revision": "b47fe71883e4a41ec331",
+    "url": "/static/js/2.e009e008.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a3766a85e052c9367ed880052819c51e",
+    "url": "/index.html"
+  }
+];
