c41f0829021380e71664f81955542a6a4064a3de
Faizan Fazal <39065186+ffazal@users.noreply.github.com>
2022-06-07 13:42:39 -0400

    Updates

---
+++ precache-manifest.ee66c20d93c24dfa76dd9abc017f11ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "deb996f54332e525f912",
+    "url": "/ffazal/robofriends/static/css/main.3eb9ce58.chunk.css"
+  },
+  {
+    "revision": "deb996f54332e525f912",
+    "url": "/ffazal/robofriends/static/js/main.45bfddca.chunk.js"
+  },
+  {
+    "revision": "f2a10c124870282ae92d",
+    "url": "/ffazal/robofriends/static/js/runtime~main.2e6594b2.js"
+  },
+  {
+    "revision": "77d2bcaa9a72229f1cfe",
+    "url": "/ffazal/robofriends/static/css/2.c3e5aa5d.chunk.css"
+  },
+  {
+    "revision": "77d2bcaa9a72229f1cfe",
+    "url": "/ffazal/robofriends/static/js/2.738473dc.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/ffazal/robofriends/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ffazal/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "87c58a197f250db48abc0faf7358663f",
+    "url": "/ffazal/robofriends/index.html"
+  }
+];
